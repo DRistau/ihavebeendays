f2eb78eebedc19add0ce1eebd3bdc1d6b56bb48b
Klaus Laube <klaus@corp.globo.com>
2015-02-09 22:38:58 -0200

    Addind grunt-sass, grunt-contrib-watch and grunt-autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,39 @@
 module.exports = function(grunt) {
     var config = {
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        appPath: 'ibeendays'
     };
 
-    grunt.registerTask('default', []);
+    config.sass = {
+        options: {
+            sourceMap: true,
+            outputStyle: 'expanded'
+        },
+        dist: {
+            files: {
+                '<%= appPath %>/static/css/styles.css': '<%= appPath %>/sass/styles.scss'
+            }
+        }
+    };
+
+    config.autoprefixer = {
+        no_dest_single: {
+            src: '<%= appPath %>/static/css/styles.css',
+        }
+    };
+
+    config.watch = {
+        sass: {
+            files: ['<%= appPath %>/sass/**/*.{scss,sass}', '<%= appPath %>/sass/partials/**/*.{scss,sass}'],
+            tasks: ['sass:dist', 'autoprefixer']
+        }
+    };
+
+    grunt.initConfig(config);
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'watch:sass']);
 };
