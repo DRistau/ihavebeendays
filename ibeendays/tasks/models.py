d55f261b59b9a7ec4e21b3f68e6e54cb086d1b9a
Klaus Laube <klaus@corp.globo.com>
2015-05-04 23:40:39 -0300

    Creating querysets for finished and unfinished tasks

--- ibeendays/tasks/models.py
+++ ibeendays/tasks/models.py
@@ -2,11 +2,22 @@ from django.conf import settings
 from django.db import models
 
 
+class TaskQuerySet(models.QuerySet):
+
+    def finished(self):
+        return self.filter(finished_at__isnull=False)
+
+    def unfinished(self):
+        return self.filter(finished_at__isnull=True)
+
+
 class Task(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=255)
     started_at = models.DateTimeField(auto_now_add=True)
     finished_at = models.DateTimeField(blank=True, null=True)
+
+    objects = TaskQuerySet.as_manager()
 
     def __str__(self):
         return self.title
--- ibeendays/tasks/tests/test_models.py
+++ ibeendays/tasks/tests/test_models.py
@@ -1,2 +1,18 @@
+import pytest
+from datetime import datetime
+from ibeendays.tasks.factories import TaskFactory
+from ibeendays.tasks.models import Task
+
+
 def test_task_has_str_representation(task):
     assert str(task) == 'Task 0'
+
+
+@pytest.mark.django_db
+def test_bring_only_finished_tasks(tasks):
+    TaskFactory.create(finished_at=datetime.now())
+    assert Task.objects.finished().count() == 1
+
+
+def test_bring_only_unfinished_tasks(tasks):
+    assert Task.objects.unfinished().count() == 3
