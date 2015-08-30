06b0109c3582d119f37968fae48a03c3c94c8772
Klaus Laube <klaus@corp.globo.com>
2015-08-30 13:52:49 -0300

    The task detail view shows information just to the owner of the tasks

--- ihavebeendays/tasks/api.py
+++ ihavebeendays/tasks/api.py
@@ -9,6 +9,9 @@ class UserObjectsOnlyAuthorization(Authorization):
     def read_list(self, object_list, bundle):
         return object_list.filter(user=bundle.request.user)
 
+    def read_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
 
 class TaskResource(ModelResource):
     def prepend_urls(self):
--- ihavebeendays/tasks/tests/test_api.py
+++ ihavebeendays/tasks/tests/test_api.py
@@ -9,7 +9,7 @@ class TestTaskListApi:
         response = client.get(self.URL)
         assert response.status_code == 401
 
-    def test_access_the_task_list(self, logged_in_request, unfinished_tasks):
+    def test_access_the_list(self, logged_in_request, unfinished_tasks):
         response = logged_in_request(self.URL)
         assert response.status_code == 200
 
@@ -28,3 +28,31 @@ class TestTaskListApi:
 
         assert len(response_as_json['objects']) == 0
 
+
+class TestTaskDetailApi:
+    URL = '/api/v1/tasks/{0}/?format=json'
+
+    def test_access_as_anonymous_user_shows_error(self, client, unfinished_tasks):
+        response = self._get_from_task(client.get, unfinished_tasks[0])
+        assert response.status_code == 401
+
+    def test_access_the_details(self, logged_in_request, unfinished_tasks):
+        response = self._get_from_task(logged_in_request, unfinished_tasks[0])
+        assert response.status_code == 200
+
+    def test_logged_access_shows_the_task_details(self, logged_in_request, unfinished_tasks):
+        response = self._get_from_task(logged_in_request, unfinished_tasks[0])
+        response_as_json = json.loads(response.content.decode())
+
+        assert response_as_json['title'] == 'Unfinished Task'
+
+    def test_dont_see_tasks_from_another_user(self, logged_in_request, unfinished_tasks):
+        unfinished_tasks[0].user = UserFactory.create(username='another-user')
+        unfinished_tasks[0].save()
+
+        response = self._get_from_task(logged_in_request, unfinished_tasks[0])
+
+        assert response.status_code == 401
+
+    def _get_from_task(self, client_fn, task):
+        return client_fn(self.URL.format(task.uuid))
